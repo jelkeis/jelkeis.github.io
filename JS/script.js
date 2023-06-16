4e2618e8ae51b1d0be21b78b6fdb7b59981bb125
jelkeis <juanimeyfacu@gmail.com>
2023-06-16 12:32:39 -0300

    update
    javascript / btn cards update

--- JS/script.js
+++ JS/script.js
@@ -1,21 +1,61 @@
-function centerLastCards() {      /* MX AUTO EN LAS CARDS FINALES, TENGO QUE PONER QUE TOME TODOS LOS CARDS Y HACER %3 PARA SACAR SI QUEDAN 2 O 1 EXTRAS */
-  let cards = document.getElementsByClassName("allCards");
-  for (let i = 0; i < cards.length; i++) {
-    let card = cards[i];
-    let amountOfCards = i;
-    switch (amountOfCards%3) {
-      case 1:
-        let all = document.querySelector("body")
-        all.style.display = "none";
-        break;
-      case 2:
+const cards = document.getElementsByClassName("allCards");
+const flipcard = document.getElementsByClassName("flip-card");
+const animeImg = document.getElementsByClassName("animeImg");
+const h1Anime = document.getElementsByClassName("h1Anime");
+let h1AnimeValue;  /* igualar al valor de h1Anime para poder reemplazarlo luego */
+let flipCardBtn = document.querySelectorAll('.flipCardBtn');
 
-        break;
-    }
-  }
+switch (cards.length % 3) {
+	case 1:
+		cards[cards.length - 1].style.margin = "0 auto";
+		break;
+	case 2:
+		cards[cards.length - 1].style.margin = "0 auto";
+		cards[cards.length - 2].style.margin = "0 auto";
+		break;
 }
 
 
-function expandCards() {
+flipCardBtn.forEach(function(flipCardBtn) {
+  let flipCardFather = flipCardBtn.closest('.flip-card');
 
-}
+  flipCardBtn.addEventListener('click', function() {
+    flipCardFather.classList.toggle('expandCard');
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* function expandCards() {
+	cards[8].classList.replace("col-4", "col-12"); - para cambiar la imagen usar el nombre del h1, la primera letra en mayusculas
+	flipcard[9].classList.add("expandCard")
+} */
+
+
+
+/* .replace("Mairimashita! ", "");
+.replace("One Piece: Episode A", "onePieceEpisodeA");
+.replace("spy x family", "spyXFamily");
+.replace(" ", "");
+.replace(":", "");
+.replace(";", "");
+.replace("/", "");
+.replace("", ""); */
+
+/* 	
+		cambiar col-4 por col-12
+		cambiar height y width
+		cambiar background
+*/
