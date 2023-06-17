d379c1d3293e31e8d99e4771307894f0c487c5e7
jelkeis <juanimeyfacu@gmail.com>
2023-06-17 01:42:24 -0300

    updates
    js updates

--- JS/script.js
+++ JS/script.js
@@ -1,29 +1,77 @@
 const cards = document.getElementsByClassName("allCards");
-const flipcard = document.getElementsByClassName("flip-card");
-const animeImg = document.getElementsByClassName("animeImg");
-const h1Anime = document.getElementsByClassName("h1Anime");
-let h1AnimeValue;  /* igualar al valor de h1Anime para poder reemplazarlo luego */
 let flipCardBtn = document.querySelectorAll('.flipCardBtn');
+let closeClickBtn = document.querySelectorAll('.closeClickBtn');
 
 switch (cards.length % 3) {
-	case 1:
-		cards[cards.length - 1].style.margin = "0 auto";
-		break;
-	case 2:
-		cards[cards.length - 1].style.margin = "0 auto";
-		cards[cards.length - 2].style.margin = "0 auto";
-		break;
+  case 1:
+    cards[cards.length - 1].style.margin = "0 auto";
+    break;
+  case 2:
+    cards[cards.length - 1].style.margin = "0 auto";
+    cards[cards.length - 2].style.margin = "0 auto";
+    break;
 }
 
 
-flipCardBtn.forEach(function(flipCardBtn) {
-  let flipCardFather = flipCardBtn.closest('.flip-card');
+flipCardBtn.forEach(function (flipCardBtn) {
+  let allCards = flipCardBtn.closest('.allCards');
+  let flipCardClick = flipCardBtn.parentElement.previousElementSibling;
+  let flipCard = flipCardBtn.parentElement.parentElement.parentElement;
+  let bgImgVar = flipCardBtn.closest('.flip-card-back').querySelector('h1').textContent
+    .replace("Mairimashita! ", "")
+    .replace("One Piece: Episode A", "onePieceEpisodeA")
+    .replace("spy x family", "spyXFamily")
+    .replace("no", "No")
+    .replace(/ /g, "")
+    .replace(":", "")
+    .replace(";", "")
+    .replace("/", "")
+    .replace("", "");
+  bgImgVar = bgImgVar.charAt(0).toLowerCase() + bgImgVar.slice(1);
 
-  flipCardBtn.addEventListener('click', function() {
-    flipCardFather.classList.toggle('expandCard');
+
+  flipCardBtn.addEventListener('click', function () {
+    flipCard.classList.add('expandCard');
+    allCards.classList.add('position-relative');
+
+
+    allCards.classList.replace('col-4', 'col-12');
+
+
+    flipCardClick.style.backgroundImage = `url(../IMG/wallpapers/animes/${bgImgVar}.jpg)`;
+
+
+
+
+    flipCardClick.classList.replace('d-none', 'd-block');
+    flipCard.classList.remove('flip-card')
+
   });
+
+
+
+
 });
 
+closeClickBtn.forEach(function (closeClickBtn) {
+  let flipCard = closeClickBtn.parentElement.parentElement.parentElement.parentElement;
+  let allCards = closeClickBtn.closest('.allCards')
+  let flipCardClick = closeClickBtn.parentElement.parentElement;
+
+
+  closeClickBtn.addEventListener('click', function () {
+    flipCard.classList.remove('expandCard');
+    flipCard.classList.add('flip-card');
+    allCards.classList.remove('position-relative');
+    allCards.classList.replace('col-12', 'col-4');
+    flipCardClick.classList.replace('d-block', 'd-none');
+
+  })
+
+
+
+
+})
 
 
 
@@ -32,30 +80,4 @@ flipCardBtn.forEach(function(flipCardBtn) {
 
 
 
-
-
-
-
-
-
-/* function expandCards() {
-	cards[8].classList.replace("col-4", "col-12"); - para cambiar la imagen usar el nombre del h1, la primera letra en mayusculas
-	flipcard[9].classList.add("expandCard")
-} */
-
-
-
-/* .replace("Mairimashita! ", "");
-.replace("One Piece: Episode A", "onePieceEpisodeA");
-.replace("spy x family", "spyXFamily");
-.replace(" ", "");
-.replace(":", "");
-.replace(";", "");
-.replace("/", "");
-.replace("", ""); */
-
-/* 	
-		cambiar col-4 por col-12
-		cambiar height y width
-		cambiar background
-*/
+/* si es divisible 3 hacer que los dos de atras bajen y un break, si es divisible 2 hacer que bajen el de atras y adelante y un break, y si es divisible a 1 hacer que bajen los dos de adelante */
