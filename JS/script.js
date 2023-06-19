4b8abc4c46540593fb49f8fd0b34d0419e30faaf
jelkeis <juanimeyfacu@gmail.com>
2023-06-18 23:07:16 -0300

    update
    update

--- JS/script.js
+++ JS/script.js
@@ -13,6 +13,8 @@ switch (cards.length % 3) {
 }
 
 
+
+
 flipCardBtn.forEach(function (flipCardBtn) {
   let allCards = flipCardBtn.closest('.allCards');
   let flipCardClick = flipCardBtn.parentElement.previousElementSibling;
@@ -28,17 +30,20 @@ flipCardBtn.forEach(function (flipCardBtn) {
     .replace("/", "")
     .replace("", "");
   bgImgVar = bgImgVar.charAt(0).toLowerCase() + bgImgVar.slice(1);
+  let siblingCard = parseInt(allCards.id.slice(4));
+  /* let auxCard1;
+  let auxCard2; */
 
 
   flipCardBtn.addEventListener('click', function () {
     flipCard.classList.add('expandCard');
-    allCards.classList.add('position-relative');
+    allCards.classList.add('position-absolute');
 
 
     allCards.classList.replace('col-4', 'col-12');
 
 
-    flipCardClick.style.backgroundImage = `url(../IMG/wallpapers/animes/${bgImgVar}.jpg)`;
+    flipCardClick.style.backgroundImage = `url(../../IMG/wallpapers/animes/${bgImgVar}.jpg)`;
 
 
 
@@ -46,8 +51,33 @@ flipCardBtn.forEach(function (flipCardBtn) {
     flipCardClick.classList.replace('d-none', 'd-block');
     flipCard.classList.remove('flip-card')
 
-  });
 
+    switch (siblingCard % 3) {
+      case 1 /* primera columna */:
+        document.getElementById(`card${siblingCard+1}`).classList.add('toLowerCard');
+        document.getElementById(`card${siblingCard+2}`).classList.add('toLowerCard');
+        document.getElementById(`card${siblingCard+1}`).style.position = 'relative';
+        document.getElementById(`card${siblingCard+2}`).style.position = 'relative';
+        flipCard.closest('.row').classList.add('asdaa');
+       /*  document.getElementById(`card${siblingCard+1}`).style.add('start-50');
+        document.getElementById(`card${siblingCard+2}`).style.add('start-50'); */
+        break;
+
+      case 2 /* segunda columna */:
+        document.getElementById(`card${siblingCard-1}`).classList.add('toLowerCard');
+        document.getElementById(`card${siblingCard+1}`).classList.add('toLowerCard');
+        document.getElementById(`card${siblingCard-1}`).style.position = 'relative';
+        document.getElementById(`card${siblingCard+1}`).style.position = 'relative';
+        break;
+
+      case 0 /* tercera columna */:
+        document.getElementById(`card${siblingCard-2}`).classList.add('toLowerCard');
+        document.getElementById(`card${siblingCard-1}`).classList.add('toLowerCard');
+        document.getElementById(`card${siblingCard-2}`).style.position = 'relative';
+        document.getElementById(`card${siblingCard-1}`).style.position = 'relative';
+        break;
+    }
+  });
 
 
 
@@ -62,13 +92,11 @@ closeClickBtn.forEach(function (closeClickBtn) {
   closeClickBtn.addEventListener('click', function () {
     flipCard.classList.remove('expandCard');
     flipCard.classList.add('flip-card');
-    allCards.classList.remove('position-relative');
+    allCards.classList.remove('position-absolute');
     allCards.classList.replace('col-12', 'col-4');
     flipCardClick.classList.replace('d-block', 'd-none');
 
   })
-
-
 
 
 })
